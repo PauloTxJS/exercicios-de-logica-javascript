0d4e4f4fa80a2b691312d3051360f01064540616
lauraol <lauraoliveirape@gmail.com>
2019-12-02 15:50:28 -0300

    primeiro teste com aúdio

---
+++ testeAudioVideo/testeAudio.js
@@ -0,0 +1,32 @@
+var numMusica = 0;
+
+function tocaAnterior() {
+    numMusica--;
+
+    if (numMusica < 0) {
+        numMusica = 3;
+    }
+    document.getElementById("mymusic").src = "./Musica" + numMusica + ".mp3";
+    document.getElementById("mymusic").play();
+}
+function tocaAtual() {
+    console.log("musica atual = " + document.getElementById("mymusic").src);
+
+    document.getElementById("mymusic").src = "./Musica" + numMusica + ".mp3";
+    document.getElementById("mymusic").play();
+    console.log("ah... chato!");
+}
+function paraTudo() {
+    document.getElementById("mymusic").pause();
+
+}
+function tocaProxima() {
+    numMusica++
+
+    if (numMusica == 4) {
+        numMusica = 0;
+    }
+    document.getElementById("mymusic").src = "./Musica" + numMusica + ".mp3";
+    document.getElementById("mymusic").play();
+
+}
